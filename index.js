920fadde5f7aa70f94d6dd589bea4f55f377296f
umedjon01 <najottalimfn15@gmail.com>
2024-01-18 16:46:36 +0500

    Homework

---
+++ index.js
@@ -0,0 +1,103 @@
+//  1-chi masala
+
+// let num = 1234;
+
+// let birlar = num % 10;
+// let on = num % 100;
+// let yuz = num % 1000;
+// let ming = num % 10000;
+// let onlar = (on - birlar) / 10;
+// let yuzlar = (yuz - on) / 100;
+// let minglar = (ming - yuz) / 1000;
+// let son = 0;
+// if (birlar % 2 == 0) {
+//   son = son + 1;
+// }
+// if (onlar % 2 == 0) {
+//   son = son + 1;
+// }
+// if (yuzlar % 2 == 0) {
+//   son = son + 1;
+// }
+// if (minglar % 2 == 0) {
+//   son = son + 1;
+// }
+// console.log("Juftsonlar soni   " + son + "ta");
+
+// 2-chi masala
+
+// let num = 235;
+
+// let birlar = num % 10;
+// let on = num % 100;
+// let yuz = num % 1000;
+// let onlar = (on - birlar) / 10;
+// let yuzlar = (yuz - on) / 100;
+// let res = yuzlar + onlar + birlar;
+
+// if (res%2==0  && res%5==0){
+//     console.log(true)
+// }else {
+//     console.log(false);
+// }
+
+// 3-chi masala
+
+// let num1 = 7;
+// let num2 = 4;
+// let num3 = 5;
+
+// if ((num1 < num2 && num1 > num3) || (num1 > num2 && num1 < num3)) {
+//   console.log("O'rtacha son" + num1);
+// } else if ((num2 < num1 && num2 > num3) || (num2 > num1 && num2 < num3)) {
+//   console.log("O'rtacha son" + num2);
+// } else if ((num3 < num1 && num3 > num2) || (num3 > num1 && num3 < num2)) {
+//   console.log("O'rtacha son    " + num3);
+// }
+
+//  4-chi masala
+
+// let num = 1001;
+
+// let birlar = num % 10;
+// let on = num % 100;
+// let yuz = num % 1000;
+// let ming = num % 10000;
+// let onlar = (on - birlar) / 10;
+// let yuzlar = (yuz - on) / 100;
+// let minglar = (ming - yuz) / 1000;
+
+// if (minglar == birlar && yuzlar == onlar) {
+//   console.log("UShbu kiritilgan son palindrom");
+// } else if (yuzlar == birlar && num < 1000 && num > 99) {
+//   console.log("UShbu kiritilgan son palindrom");
+// } else if (onlar == birlar && num < 100) {
+//   console.log("UShbu kiritilgan son palindrom");
+// } else {
+//   console.log("UShbu kiritilgan son palindrom emas");
+// }
+
+//  5-chi masala
+
+// let num = 15;
+// let num = 5;
+
+// let res = (num + num) % 24;
+// console.log(res);
+
+// 6-chi mas'ala
+
+// let sum = 9732739; // +prompt("O'zingizda bor mablag'bi kiriting");
+// let dol = 500; //+prompt("Borib kelish bilet natxini kiriting  $");
+// let hotel = 250; //+prompt("Mehmonxona tulovini kiriting ");
+// let travel = 120; // +prompt("Muzey va ko'ngil ochar joylar uchun xarajatni kiriting ");
+// let dollar = (dol + hotel) * 11000.34;
+// let travell = travel * 12354.03;
+// let res = dollar + travell;
+// let result = sum - res;
+
+// if (result >= 0) {
+//   console.log("Oq yo'l Alisher!");
+// } else {
+//   console.log("Alisher,ozgina sabr qilish kerak bo'lar ekan");
+// }
